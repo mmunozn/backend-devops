1ce69eb4c6e02ac31ff815a13851c61bdfed1056
m_munozn <m_munozn@inacap.cl>
2024-12-14 00:25:10 -0300

    Descripción de tus cambios

--- src/app/calculadora.ts
+++ src/app/calculadora.ts
@@ -6,13 +6,16 @@ function operar(operacion: string = "", a: number, b: number) {
     }
 }
 
-function suma(a: number, b: number) {
+function suma(a: number, b: number) { //a = 1 , b=2
+
     if (a === undefined || b === undefined) {
         throw new Error("No se puede sumar indefinidos");
     }
+
     if (typeof a !== 'number' || typeof b !== 'number') {
         return NaN;
     }
+
     return a + b;
 }
 
